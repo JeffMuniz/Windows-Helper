fe2c1361842a40cc1b665ffa46144225bc033e42
JeffMuniz <34946878+JeffMuniz@users.noreply.github.com>
2024-07-08 14:22:51 -0300

    before process

--- debloat-win-10.ps1
+++ debloat-win-10.ps1
@@ -1,14 +1,11 @@
-# Install DISM
 
+
+
+Install DISM
 DISM.exe /Online /Cleanup-image /Restorehealth
 DISM /Online /Get-ProvisionedAppxPackages | select-string "Packagename"
+
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:PACKAGENAME
-
-
-
-
-
-
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.BingWeather_4.53.33420.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Clipchamp.Clipchamp_2.2.8.0_neutral_~_yxz26nhyzhsrt
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.BingNews_4.2.27001.0_neutral_~_8wekyb3d8bbwe
@@ -107,6 +104,39 @@ DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxGamingOve
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxIdentityProvider_12.90.14001.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.XboxSpeechToTextOverlay_1.21.13002.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.YourPhone_1.22052.554.0_neutral_~_8wekyb3d8bbwe
+
+
+
+
+Get-AppxPackage -name “Microsoft.ZuneMusic” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Music.Preview” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.XboxGameCallableUI” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.XboxIdentityProvider” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingTravel” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingHealthAndFitness” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingFoodAndDrink” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.People” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingFinance” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.3DBuilder” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsCalculator” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingNews” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.XboxApp” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingSports” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsCamera” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Getstarted” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Office.OneNote” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsMaps” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.MicrosoftSolitaireCollection” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.MicrosoftOfficeHub” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingWeather” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BioEnrollment” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsStore” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Windows.Photos” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsPhone” | Remove-AppxPackage
+
+
+
+DISM /Online /Get-ProvisionedAppxPackages | select-string "Packagename"
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.Xbox.TCUI_1.23.28004.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.YourPhone_1.23032.186.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:MicrosoftTeams_23091.406.2009.3890_x64__8wekyb3d8bbwe
@@ -210,6 +240,211 @@ DISM /Online /Remove-ProvisionedAppxPackage /PackageName:MicrosoftCorporationII.
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.WindowsAlarms_2022.2209.11.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:microsoft.windowscommunicationsapps_16005.14326.20970.0_neutral_~_8wekyb3d8bbwe
 DISM /Online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.WindowsFeedbackHub_2022.317.2207.0_neutral_~_8wekyb3d8bbwe
+
+
+
+Remove Windows 10 apps
+Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage *XboxApp* | Remove-AppxPackage
+Get-AppxPackage ** | Remove-AppxPackage
+
+
+#reinstall store
+Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+wsl --set-default-version 2
+
+
+DISM /Online /Remove-ProvisionedAppxPackage /PackageName:PACKAGENAME
+<command> is a *placeholder* for any valid command; e.g.:
+#    $cmdOutput = Get-Date
+#    $cmdOutput = attrib.exe +R readonly.txt
+#    $cmdOutput = <command> captures the command's success stream / stdout output
+$cmdOutput = DISM /Online /Get-ProvisionedAppxPackages | select-string Packagename
+#DISM /Online /Remove-ProvisionedAppxPackage /PackageName:$cmdOutput
+
+
+
+Reg Delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
+Reg Delete "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
+
+
+Get-AppxPackage -AllUsers| Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml”}
+Use the 
+Get-AppXPackage | Foreach command to add a new AppX package and disable development
+ mode before registering the “$($_.InstallLocation)AppXManifest.xml” in each of the AppX packages.
+It will take time to complete step three. The corrupt file will be replaced in the system as a result.
+Step 3: Restart your computer after following these instructions.
+ This solution can quickly fix the unexpected store exception error if any of your system files are corrupt.
+install
+
+OLD!^!^!^!^!
+
+with PowerShell
+Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Music.Preview” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.XboxIdentityProvider” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingTravel” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingHealthAndFitness” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingFoodAndDrink” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.People” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingFinance” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.3DBuilder” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsCalculator” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingNews” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.XboxApp” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingSports” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.WindowsCamera” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Getstarted” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Office.OneNote” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.MicrosoftSolitaireCollection” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.MicrosoftOfficeHub” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.BingWeather” | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.                         dMusic” | Remove-AppxPackage
+Get-AppxPackage *Microsoft.                         d* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage *pad* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage
+Get-AppxPackage *fac* | Remove-AppxPackage
+Get-AppxPackage *f* | Remove-AppxPackage
+Get-AppxPackage -name “Microsoft.Music.Preview” | Remove-AppxPackage-AppxPackage
+Remove-AppxPackage-AppxPackage -name “Microsoft.XboxIdentityProvider” | Remove-AppxPackage-AppxPackage -name “Microsoft.BingTravel” |
+Remove-AppxPackage-AppxPackage -name “Microsoft.BingHealthAndFitness” | Remove-AppxPackage-AppxPackage -name “Microsoft.BingFoodAndDrink” |
+Remove-AppxPackage-AppxPackage -name “Microsoft.People” | Remove-AppxPackage-AppxPackage -name “Microsoft.BingFinance” |
+Remove-AppxPackage-AppxPackage -name “Microsoft.3DBuilder” | Remove-AppxPackage-AppxPackage -name “Microsoft.WindowsCalculator” 
+| Remove-AppxPackage-AppxPackage -name “Microsoft.BingNews” | Remove-AppxPackage-AppxPackage -name “Microsoft.XboxApp” |
+Remove-AppxPackage-AppxPackage -name “Microsoft.BingSports” | Remove-AppxPackage-AppxPackage -name “Microsoft.WindowsCamera” | Remove-AppxPackage-AppxPackage -name “Microsoft.Getstarted” | Remove-AppxPackage-AppxPackage -name “Microsoft.Office.OneNote” | Remove-AppxPackage-AppxPackage -name “Microsoft.MicrosoftSolitaireCollection” | Remove-AppxPackage-AppxPackage -name “Microsoft.MicrosoftOfficeHub” 
+| Remove-AppxPackage-AppxPackage -name “Microsoft.BingWeather” | Remove-AppxPackage-AppxPackage -name “Microsoft.BioEnrollment” |
+Remove-AppxPackage-AppxPackage -name “Microsoft.WindowsStore” | Remove-AppxPackage-AppxPackage -name “Microsoft.WindowsPhone” | Remove-AppxPackage
+Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Select-String "Microsoft."
+Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Select-String "Microsoft." | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage *XboxApp* | Remove-AppxPackage-AppxPackage ** | Remove-AppxPackage
+
+
+DISM /Online /Get-ProvisionedAppxPackages | select-string "Packagename"
+Reinstall Store
+Get-AppxPackage -allusers Microsoft.WindowsStore | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+Reg Delete "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
+Use the Get-AppXPackage | Foreach command to add a new AppX package and disable development
+mode before registering the “$($_.InstallLocation)AppXManifest.xml” in each of the AppX packages.
+It will take time to complete step three. The corrupt file will be replaced in the system as a result.
+Step 3: Restart your computer after following these instructions. 
+This solution can quickly fix the unexpected store exception error if any of your system files are corrupt.
+
+
+
+Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Select-String "Microsoft."
+
+Microsoft.AAD.BrokerPlugin_1000.19580.1000.0_neutral_neutral_cw5n1h2txyewy
+Microsoft.AccountsControl_10.0.22621.1_neutral__cw5n1h2txyewy
+Microsoft.AsyncTextService_10.0.22621.1_neutral__8wekyb3d8bbwe
+Microsoft.BioEnrollment_10.0.19587.1000_neutral__cw5n1h2txyewy
+Microsoft.CredDialogHost_10.0.19595.1001_neutral__cw5n1h2txyewy
+Microsoft.MicrosoftEdgeDevToolsClient_1000.22621.1.0_neutral_neutral_8wekyb3d8bbwe
+Microsoft.UI.Xaml.CBS_8.2206.15001.0_x64__8wekyb3d8bbwe
+Microsoft.Win32WebViewHost_10.0.22621.1_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.Apprep.ChxApp_1000.22621.1.0_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.CallingShellApp_1000.22621.1.0_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.CapturePicker_10.0.19580.1000_neutral__cw5n1h2txyewy
+Microsoft.Windows.NarratorQuickStart_10.0.22621.1_neutral_neutral_8wekyb3d8bbwe
+Microsoft.Windows.OOBENetworkCaptivePortal_10.0.21302.1000_neutral__cw5n1h2txyewy
+Microsoft.Windows.OOBENetworkConnectionFlow_10.0.21302.1000_neutral__cw5n1h2txyewy
+Microsoft.Windows.ParentalControls_1000.22621.1.0_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.PeopleExperienceHost_10.0.22621.1_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.PinningConfirmationDialog_1000.22621.1.0_neutral__cw5n1h2txyewy
+Microsoft.Windows.PrintQueueActionCenter_1.0.1.0_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.XGpuEjectDialog_10.0.22621.1_neutral_neutral_cw5n1h2txyewy
+Microsoft.XboxGameCallableUI_1000.22621.1.0_neutral_neutral_cw5n1h2txyewy
+MicrosoftWindows.Client.Core_1000.22633.1000.0_x64__cw5n1h2txyewy
+MicrosoftWindows.UndockedDevKit_10.0.22621.1_neutral_neutral_cw5n1h2txyewy
+Microsoft.549981C3F5F10_3.2204.14815.0_x64__8wekyb3d8bbwe
+Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe
+Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe
+Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe
+Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe
+Microsoft.BingNews_1.0.1.0_x64__8wekyb3d8bbwe
+Microsoft.BingWeather_1.0.6.0_x64__8wekyb3d8bbwe
+Microsoft.GamingApp_2105.900.24.0_x64__8wekyb3d8bbwe
+Microsoft.MicrosoftOfficeHub_18.2204.1141.0_x64__8wekyb3d8bbwe
+Microsoft.MicrosoftSolitaireCollection_4.12.3171.0_x64__8wekyb3d8bbwe
+Microsoft.MicrosoftStickyNotes_4.0.422.0_x64__8wekyb3d8bbwe
+Microsoft.People_10.1909.12456.0_x64__8wekyb3d8bbwe
+Microsoft.PowerAutomateDesktop_1.0.219.0_x64__8wekyb3d8bbwe
+Microsoft.ScreenSketch_11.2201.12.0_x64__8wekyb3d8bbwe
+Microsoft.Todos_0.54.42772.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsAlarms_1.0.75.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsCalculator_10.2103.8.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsCamera_2022.2201.4.0_x64__8wekyb3d8bbwe
+microsoft.windowscommunicationsapps_16005.14326.20544.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsFeedbackHub_1.2201.61.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsMaps_1.0.39.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsNotepad_11.2112.32.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsSoundRecorder_1.0.42.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsTerminal_1.12.10983.0_x64__8wekyb3d8bbwe
+Microsoft.Xbox.TCUI_1.23.28004.0_x64__8wekyb3d8bbwe
+Microsoft.XboxGameOverlay_1.47.2385.0_x64__8wekyb3d8bbwe
+Microsoft.XboxIdentityProvider_12.50.6001.0_x64__8wekyb3d8bbwe
+Microsoft.XboxSpeechToTextOverlay_1.17.29001.0_x64__8wekyb3d8bbwe
+MicrosoftCorporationII.MicrosoftFamily_0.1.28.0_x64__8wekyb3d8bbwe
+MicrosoftCorporationII.QuickAssist_2.0.5.0_x64__8wekyb3d8bbwe
+MicrosoftTeams_24102.2309.2851.4917_x64__8wekyb3d8bbwe
+Microsoft.MicrosoftEdge.Stable_124.0.2478.105_neutral__8wekyb3d8bbwe
+Microsoft.VCLibs.140.00_14.0.33519.0_x64__8wekyb3d8bbwe
+Microsoft.VCLibs.140.00_14.0.33519.0_x86__8wekyb3d8bbwe
+Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x86__8wekyb3d8bbwe
+Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x86__8wekyb3d8bbwe
+Microsoft.NET.Native.Runtime.2.1_2.1.26424.0_x86__8wekyb3d8bbwe
+Microsoft.NET.Native.Runtime.2.1_2.1.26424.0_x64__8wekyb3d8bbwe
+Microsoft.NET.Native.Framework.2.1_2.1.27427.0_x86__8wekyb3d8bbwe
+Microsoft.NET.Native.Framework.2.1_2.1.27427.0_x64__8wekyb3d8bbwe
+Microsoft.UI.Xaml.2.8_8.2310.30001.0_x64__8wekyb3d8bbwe
+Microsoft.UI.Xaml.2.8_8.2310.30001.0_x86__8wekyb3d8bbwe
+Microsoft.VCLibs.140.00.UWPDesktop_14.0.33519.0_x86__8wekyb3d8bbwe
+Microsoft.VCLibs.140.00.UWPDesktop_14.0.33519.0_x64__8wekyb3d8bbwe
+MicrosoftWindows.Client.WebExperience_424.1301.450.0_x64__cw5n1h2txyewy
+Microsoft.Windows.Photos_2024.11030.15001.0_x64__8wekyb3d8bbwe
+Microsoft.Paint_11.2403.28.0_x64__8wekyb3d8bbwe
+Microsoft.ZuneMusic_11.2403.5.0_x64__8wekyb3d8bbwe
+Microsoft.UI.Xaml.2.7_7.2208.15002.0_x64__8wekyb3d8bbwe
+Microsoft.UI.Xaml.2.7_7.2208.15002.0_x86__8wekyb3d8bbwe
+Microsoft.ZuneVideo_10.22091.10061.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsAppRuntime.CBS_4000.1054.1457.0_x64__8wekyb3d8bbwe
+MicrosoftWindows.Client.FileExp_1000.22653.1001.0_x64__cw5n1h2txyewy
+MicrosoftWindows.Client.LKG_1000.22621.3593.0_x64__cw5n1h2txyewy
+Microsoft.UI.Xaml.CBS_8.2305.16002.0_x64__8wekyb3d8bbwe
+MicrosoftWindows.Client.Core_1000.22653.1002.0_x64__cw5n1h2txyewy
+Microsoft.ECApp_10.0.22621.3085_neutral__8wekyb3d8bbwe
+Microsoft.LockApp_10.0.22621.3235_neutral__cw5n1h2txyewy
+Microsoft.Windows.CloudExperienceHost_10.0.22621.2506_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.ContentDeliveryManager_10.0.22621.2506_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.ShellExperienceHost_10.0.22621.3527_neutral_neutral_cw5n1h2txyewy
+Microsoft.Windows.StartMenuExperienceHost_10.0.22621.2506_neutral_neutral_cw5n1h2txyewy
+MicrosoftWindows.Client.CBS_1000.22700.1003.0_x64__cw5n1h2txyewy
+Microsoft.WindowsAppRuntime.1.5_5001.119.156.0_x86__8wekyb3d8bbwe
+Microsoft.Windows.DevHome_0.1300.477.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsAppRuntime.1.5_5001.119.156.0_x64__8wekyb3d8bbwe
+Microsoft.LanguageExperiencePackpt-BR_22621.52.224.0_neutral__8wekyb3d8bbwe
+Microsoft.SecHealthUI_1000.25992.9000.0_x64__8wekyb3d8bbwe
+Microsoft.HEVCVideoExtension_2.0.61931.0_x64__8wekyb3d8bbwe
+Microsoft.Getstarted_10.2312.1.0_x64__8wekyb3d8bbwe
+Microsoft.XboxGamingOverlay_7.124.3191.0_x64__8wekyb3d8bbwe
+Microsoft.VP9VideoExtensions_1.1.451.0_x64__8wekyb3d8bbwe
+Microsoft.HEIFImageExtension_1.1.861.0_x64__8wekyb3d8bbwe
+Microsoft.WebMediaExtensions_1.1.663.0_x64__8wekyb3d8bbwe
+Microsoft.GetHelp_10.2403.20861.0_x64__8wekyb3d8bbwe
+Microsoft.WindowsAppRuntime.1.4_4000.1227.1637.0_x86__8wekyb3d8bbwe
+Microsoft.WindowsAppRuntime.1.4_4000.1227.1637.0_x64__8wekyb3d8bbwe
+Microsoft.RawImageExtension_2.3.1221.0_x64__8wekyb3d8bbwe
+Microsoft.WebpImageExtension_1.1.1221.0_x64__8wekyb3d8bbwe
+Microsoft.OneDriveSync_24081.421.3.0_neutral__8wekyb3d8bbwe
+Microsoft.YourPhone_1.24041.108.0_x64__8wekyb3d8bbwe
+MicrosoftWindows.CrossDevice_1.24042.39.0_x64__cw5n1h2txyewy
+
+
+
+
+
 
 
 
